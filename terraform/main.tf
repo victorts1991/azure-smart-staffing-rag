8d73e1f080bb0c7f1764a0da799cfc1a1419a228
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2026-03-23 10:14:38 -0300

    update terraform backend storage and refresh ci/cd triggers
    - update the terraform backend storage account name in main.tf
    - add a 40-second wait step to the workflow to ensure services are ready before retrieving resource data
    - update the content of test_trigger files in the k8s, search_assets, and terraform directories to manually trigger a new pipeline run

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "ststaffingragtf9cafde"
+    storage_account_name = "ststaffingragtfc96fd4"
     container_name       = "tfstate"
     key                  = "smart-staffing.terraform.tfstate"
     }
